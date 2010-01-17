a44ba8483588e4fa4a9f45ae8231dc2a69d637cb
Seth Falcon <seth@userprimary.net>
2010-01-16 20:18:47 -0800

    Add some basic tagging and tag listing support

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,3 +2,11 @@ include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
 
+require 'time'
+
+# sort items with a :created_at timestamp in
+# reverse chronological order
+def sort_posts_by_date(posts)
+  posts.sort_by { |p| Time.parse(p[:created_at]) }.reverse
+end
+
