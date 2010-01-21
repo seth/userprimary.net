f51b1bc56e0bdcf04b63c87047c72df995488006
Seth Falcon <seth@userprimary.net>
2010-01-20 21:56:00 -0800

    Add archive by month and by tag

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -10,6 +10,15 @@ def sort_posts_by_date(posts)
   posts.sort_by { |p| Time.parse(p[:created_at]) }.reverse
 end
 
+def get_tag_items
+  @items.find { |i| i.identifier == "/tags/" }.children
+end
+
+def get_archive_items
+  archives = @items.find { |i| i.identifier == "/archives/" }.children
+  archives.sort_by { |a| Time.parse(a[:title]) }
+end
+
 def nav_link_to_unless_current(text, path)
   if @item_rep and @item_rep.path == path
     %[<span class="current">#{text}</span>]
