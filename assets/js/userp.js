a2f206e50c789e328a5fe3b4e429ac33d27cb938
Seth Falcon <seth@userprimary.net>
2010-02-07 13:55:16 -0800

    Degridify homepage

--- assets/js/userp.js
+++ assets/js/userp.js
@@ -1,6 +1,7 @@
 $('document').ready(function () {
     $('#header').corner("bottom 8px")
     $('#main').corner("8px")
+    $('#summaries').corner("8px")
     $('#footer').corner("top 8px")
     $('#coord_img').corner("6px")
     $('abbr.timeago').timeago()
