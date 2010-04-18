c76c8ff761103404de87a9c1b37ffc2b984fcc66
Seth Falcon <seth@userprimary.net>
2010-04-18 14:28:38 -0700

    Re-enable disqus comments
    Now with imported comments from old posts from Wordpress.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -141,22 +141,26 @@ def disqus_dev_mode?
   File.exist?("DEV_MODE") ? 1 : 0
 end
 
-def disqus_embed
+def disqus_embed_disable_comments
   embed = <<EOF
 <p>Comments are temporarily disabled.  If you'd like to share your thoughts, send me a note on Twitter (@sfalcon) or send me an email (contact details below).</p>
 EOF
 end
 
-def disqus_embed_save
+def disqus_embed
   embed = <<EOF
-  <script type="text/javascript">
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+  /**
+    * var disqus_identifier; [Optional but recommended: Define a unique identifier (e.g. post id or slug) for this thread] 
+    */
   (function() {
    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-   dsq.src = 'http://userprimary.disqus.com/embed.js';
+   dsq.src = 'http://userprimary-net.disqus.com/embed.js';
    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=userprimary">comments powered by Disqus.</a></noscript>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=userprimary-net">comments powered by Disqus.</a></noscript>
 <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
 EOF
   embed
